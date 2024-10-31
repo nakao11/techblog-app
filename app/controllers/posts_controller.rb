091d5b444bdba5d2d941c6d972c1337380258a91
nakao11 <pizaya_1@yahoo.com>
2024-11-01 05:42:57 +0900

    学習ログ投稿機能を作成

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  # GETリクエストで空のオブジェクトを作成し、form_withで紐づける
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      flash[:notice] = '投稿しました'
+      redirect_to root_path
+    else
+      flash[:alert] = '投稿に失敗しました'
+      render :new
+    end
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
 
+  resources :posts, only:[:new, :create]
+
   get "up" => "rails/health#show", as: :rails_health_check
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
